f6ddf58a50e494a59a8f0990728a2adfebeeedc2
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-10-05 08:32:54 +0200

    irepository

---
+++ GreetMe_DataAccess/Repsitory/IPeopleRepository.cs
@@ -0,0 +1,14 @@
+﻿using GreetMe_DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetMe_DataAccess.Repsitory
+{
+    public interface IPeopleRepository : IRepository<Person>
+    {
+        Task<string> GetFirstName(Guid id); //example Get Spesific Person name By id
+    }
+}
---
+++ GreetMe_DataAccess/Repsitory/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetMe_DataAccess.Repsitory
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> All();
+        Task<T> GetById(Guid id);
+        Task<IEnumerable<T>> Add(T entity);
+        Task<IEnumerable<T>> Delete(Guid id);
+        Task<IEnumerable<T>> Update(T entity);
+
+    }
+}
